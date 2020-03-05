610d233b6f095d7a665395e5c7216545457b74de
AnthonyMartin <anthony.martin@brightflag.com>
2020-03-05 22:51:48 +0000

    SHOUT added sign in buttons to scope

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -2,4 +2,12 @@
 var app = angular.module("Shout", []);
 app.controller("SignInController", ['$scope',function ($scope) {
 
+   $scope.signIn = function signIn() {
+        console.log("sign in");
+   }
+
+    $scope.signUp = function signUp() {
+        console.log("sign up");
+    }
+
 }])
