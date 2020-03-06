492a1b71cb4796b4a2b3d9f7416c31d03b7cb8b8
AnthonyMartin <anthony.martin@brightflag.com>
2020-03-06 00:38:50 +0000

    SHOUT landing page behaviour finalized

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -2,12 +2,31 @@
 var app = angular.module("Shout", []);
 app.controller("SignInController", ['$scope',function ($scope) {
 
+    $scope.signInSelection = true;
+    $scope.signInForm = false;
+    $scope.signUpForm = false;
+    $scope.homeActive = false;
+
    $scope.signIn = function signIn() {
+       $scope.signInSelection = false;
+       $scope.signInForm = true;
+       $scope.homeActive = true;
         console.log("sign in");
    }
 
     $scope.signUp = function signUp() {
+        $scope.signInSelection = false;
+        $scope.signUpForm = true;
+        $scope.homeActive = true;
         console.log("sign up");
+    }
+
+    $scope.home = function () {
+        $scope.signInSelection = true;
+        $scope.signInForm = false;
+        $scope.signUpForm = false;
+        $scope.homeActive = false;
+        console.log("back up!");
     }
 
 }])
